83f7d9c13cce9c960ff010d0fead54bed9cb5a3e
Aard Keimpema <keimpema@jive.eu>
2022-04-07 09:56:27 +0000

    Make Jupyter kernel work with most recent versions of Jupyter. There were two issues: 1. Our kernel inherents from IPykernel and that now uses asyncio. In do_execute make add the call to super().do_execute into the correct event loop. 2. CASA still gets essential configuration info by traversing the stack, some variables were globals in my code whereas CASA expected them to be local variables...

--- python/start_casa/start_casa.py
+++ python/start_casa/start_casa.py
@@ -2,6 +2,7 @@ import os
 import time
 import sys
 import traceback
+import asyncio
 from ipykernel.ipkernel import IPythonKernel
 from IPython.core.getipython import get_ipython
 from traitlets.config.loader import Config
@@ -90,11 +91,11 @@ class CasapyKernel(IPythonKernel):
     banner = "Jupyter wrapper for casa"
 
     def start(self):
-        super(CasapyKernel, self).start()
+        super().start()
         self.do_execute('%matplotlib inline', True, False, {}, False)
         #self.do_execute('%matplotlib ipympl', True, False, {}, False)
         for i in startup_scripts:
-           self.do_execute('%run -i {}'.format(i), True, False, {}, False)
+            self.do_execute('%run -i {}'.format(i), True, False, {}, False)
         wrappers = os.path.dirname(os.path.realpath(__file__)) + '/tasks_wrapped.py'
         self.do_execute('%run -i {}'.format(wrappers), True, False, {}, False)
         import casashell.private.config as config
@@ -111,7 +112,10 @@ class CasapyKernel(IPythonKernel):
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None,
                    allow_stdin=False):
-        result = super(CasapyKernel, self).do_execute(code, silent, store_history, user_expressions, allow_stdin)
+        future = asyncio.run_coroutine_threadsafe(
+                super().do_execute(code, silent, store_history, user_expressions, allow_stdin),
+                super().control_thread.io_loop.asyncio_loop)
+        result = future.result()
         # Traverse log, this is not ideal. It would be better to have our own logger application and
         # use that to obtain logging information.
         loglines = []

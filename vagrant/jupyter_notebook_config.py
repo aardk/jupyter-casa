497f50bb2d98894d082f300662009bcfd403de59
Aard Keimpema <keimpema@jive.eu>
2022-09-22 16:02:27 +0200

    Make jupyter accept connections from all interfaces

---
+++ vagrant/jupyter_notebook_config.py
@@ -0,0 +1,10 @@
+# Configuration file for jupyter-notebook.
+## Set the Access-Control-Allow-Origin header
+#  
+#  Use '*' to allow any origin to access your server.
+#  
+#  Takes precedence over allow_origin_pat.
+c.NotebookApp.allow_origin = '*'
+
+## The IP address the notebook server will listen on.
+c.NotebookApp.ip = '0.0.0.0'

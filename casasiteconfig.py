236e168ea65e0ecd3b0242c897d6486d74ee849f
Aard Keimpema <keimpema@jive.eu>
2024-09-25 16:26:11 +0200

    Bump OS to ubuntu 22.04
    The new ubuntu version allows us to use the lastest kern suite,
    with goodies like aoflagger, carta, wsclean, ..
    
    Unfortunately, this does mean loosing the almatasks package as
    this package is only available for python 3.8 at the moment.
    As far as I can tell almatasks only contains one task (wvrgcal),
    and this doesn't seem to be needed for end-users of alma data.

--- casasiteconfig.py
+++ casasiteconfig.py
@@ -1,7 +1,7 @@
 # Set this to point to the location where the site maintained casarundata can be found
 # by default datapath will include measurespath
 
-measurespath = "/usr/local/lib/python3.8/dist-packages/casadata/__data__"
+measurespath = "/usr/local/lib/python3.10/dist-packages/casadata/__data__"
 
 # turn off all auto updates of data
 
